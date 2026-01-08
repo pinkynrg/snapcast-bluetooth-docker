cb7c72646f79c21ce277736c3845e12fc634ba0f
Francesco Meli <francesco.meli@beefree.io>
2026-01-08 09:53:06 +0100

    fix: update snapclient, snapserver versions

--- snapserver/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-sed -i "s,^source = .*,source = librespot:///librespot?name=Spotify\&devicename=$DEVICE_NAME\&bitrate=320\&volume=100\&cache=$CACHE," /etc/snapserver.conf
-
-exec snapserver -c /etc/snapserver.conf
