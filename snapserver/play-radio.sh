214785a9efe3c87fcffebe491701dc8c060dabf3
Francesco Meli <francesco.meli@beefree.io>
2026-04-16 14:00:45 +0200

    fix: add user agent for radio requests

--- snapserver/play-radio.sh
+++ snapserver/play-radio.sh
@@ -6,4 +6,4 @@
 URL="$1"
 VOL="${2:-0.1}"
 LOGLEVEL=$([ "${RADIO_DEBUG}" = "true" ] && echo "warning" || echo "quiet")
-exec ffmpeg -hide_banner -loglevel "$LOGLEVEL" -re -i "$URL" -af "volume=$VOL" -f s16le -ar 44100 -ac 2 -
+exec ffmpeg -hide_banner -loglevel "$LOGLEVEL" -user_agent "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0" -re -i "$URL" -af "volume=$VOL" -f s16le -ar 44100 -ac 2 -
